5ed70e69cc0a357b703ea8551d4d6a20c28bab1f
Irvan Tonny Widjaya <irvantonnywidjaya789@gmail.com>
2022-01-20 18:03:43 +0700

    Update 20/01
    - Add AdminLTE Dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index',[
+      "users" => Auth::user()  
+    ]);
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
